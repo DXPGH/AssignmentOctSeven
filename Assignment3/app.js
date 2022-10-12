952e183afe888175fdb0ae4ea06e8d86e83ced8c
bach520 <nguyet_1475369@yahoo.com>
2022-10-12 15:07:09 -0500

    Update app.js

--- Assignment3/app.js
+++ Assignment3/app.js
@@ -16,30 +16,28 @@ const mouse = {
 
 let shape = "";
 
-function draw(shapes) {
-  canvas.addEventListener("mousemove", function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    statusDiv.innerHTML = mouse.x + " | " + mouse.y;
-  });
-  canvas.addEventListener("click", function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    switch (shapes) {
-      case "halfCircle":
-        drawHalfCircle();
-        break;
-      case "rectangle":
-        drawRectangle();
-        break;
-      case "circle":
-        drawCircle();
-        break;
-      default:
-        console.log("No Shape was chosen.");
-    }
-  });
+function currshape(str){
+  shape = str;
 }
+
+canvas.addEventListener("click", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  statusDiv.innerHTML = mouse.x + " | " + mouse.y;
+  switch (shape) {
+    case "halfCircle":
+      drawHalfCircle();
+      break;
+    case "rectangle":
+      drawRectangle();
+      break;
+    case "circle":
+      drawCircle();
+      break;
+    default:
+      console.log("No Shape was chosen.");
+  }
+});
 
 function drawHalfCircle() {
   ctx.fillStyle = "purple";
@@ -67,4 +65,5 @@ function drawCircle() {
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.removeEventListener("click", event);
 }
